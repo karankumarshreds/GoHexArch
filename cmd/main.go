e124efaea443f0a17ab80cae03d71ca4e30d48fe
karankumarshreds <naileditnerd@gmail.com>
2021-09-30 23:39:37 +0530

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	_"fmt"
+	_"github.com/karankumarshreds/GoHexArch/internal/ports"
+	_"github.com/karankumarshreds/GoHexArch/internal/adapters/core/arithmetic"
+)
+
+func main() {
+
+	// ports 
+	// var core ports.ArithmeticPort
+
+	// plug adapter -> port 
+	// core = arithmetic.NewAdapter()
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karankumarshreds/GoHexArch
+
+go 1.16
